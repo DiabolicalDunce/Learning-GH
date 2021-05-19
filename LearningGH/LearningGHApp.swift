8ffe8436e0489353e8502b6c57b7ee4097b1753a
Jonathan Lau <jlauu48+github@gmail.com>
2021-05-19 13:58:08 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jonathan Lau on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
